81bdbd4f9bb80c5e38103c3d1345968426646425
Muhammad Amirul <mirulid@gmail.com>
2024-12-19 21:34:54 +0700

    fix -

--- src/api/CaptionApi.js
+++ src/api/CaptionApi.js
@@ -1,75 +1,55 @@
-
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const BASE_URL = "https://muslim-pro-golang-production.up.railway.app";
+const BASE_URL = "https://json-server-vercel-tawny-eight.vercel.app";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
 });
-class CaptionApi{
-
-    static async getRandomCaption(){
-      try {
-        const resp = await axiosInstance.get("/captions/random");
-        console.log(resp);
-        return resp.data
-        
-      } catch (error) {
-        console.log(error);
-        
-      }
+class CaptionApi {
+  static async getTotalCaption() {
+    try {
+      const resp = await axiosInstance.get("/caption");
+      return resp.data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    static async createCaption(caption){
-      try {
-        const resp = await axiosInstance.post("/captions",caption)
-        console.log('====================================');
-        console.log("response create caption",resp.data);
-        console.log('====================================');
-        return resp.data
-      } catch (error) {
-        console.log(error);
-      }
+  static async createCaption(caption) {
+    try {
+      const resp = await axiosInstance.post("/captions", caption);
+      return resp.data;
+    } catch (error) {
+      console.log(error);
     }
-    
-    static async getCaptionbyId(id){
-      try {
-        const resp = await axiosInstance.get(`/captions/${id}`);
-        console.log("response get caption by id ", resp.data);
-        return resp.data
-        
+  }
 
-      } catch (error) {
-        console.log(error);
-        
-        
-      }
+  static async getCaptionbyId(id) {
+    try {
+      const resp = await axiosInstance.get(`/caption/${id}`);
+      return resp.data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    static async updateStatusCaption(id){
-      try {
-        const resp =  await axiosInstance.put(`/captions/${id}`);
-        console.log("response update status : ",resp.data);
-        return resp.data
-      } catch (error) {
-        
-      }
-    }
+  static async updateStatusCaption(id) {
+    try {
+      const resp = await axiosInstance.put(`/caption/${id}`);
+      console.log("response update status : ", resp.data);
+      return resp.data;
+    } catch (error) {}
+  }
 
-    static async deleteCaption(id){
-      try {
-        const resp = await axiosInstance.delete(`captions/${id}`);
-        console.log("response Delete caption" , resp);
-        
-      } catch (error) {
-        
-      }
-    }
-
-
+  static async deleteCaption(id) {
+    try {
+      const resp = await axiosInstance.delete(`caption/${id}`);
+      console.log("response Delete caption", resp);
+    } catch (error) {}
+  }
 }
 
 export default CaptionApi;
--- src/components/Caption.jsx
+++ src/components/Caption.jsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import axiosInstance from "../api/AxiosInstance";
 import CaptionApi from "../api/CaptionApi";
 
 export default function Caption() {
-  const { nomor } = useParams();
   const [caption, setCaption] = useState({});
-  const navigate = useNavigate();
 
-  
-  const randomCaptiion = async () => {
-    const response =  await CaptionApi.getRandomCaption()
-    console.log("response :::::" , response);
-    setCaption(response)
-    
+  const getRamdomCaption = async () => {
+    const response = await CaptionApi.getTotalCaption();
+    const totalCap = response.length;
+    var id = getRandomInt(totalCap);
+    const getCaption = await CaptionApi.getCaptionbyId(id);
+    setCaption(getCaption);
+  };
+  function getRandomInt(max) {
+    if (typeof max !== "number" || max <= 0) {
+      throw new Error("Parameter max must be a positive number");
+    }
+    return Math.floor(Math.random() * max) + 1;
   }
-  
   useEffect(() => {
-    randomCaptiion();
+    getRamdomCaption();
+    // randomCaptiion();
   }, []);
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center sm:px-40 px-2">
       <div className="bg-black bg-opacity-40 p-10 text-xl gap-3 rounded-lg text-white mb-36 mt-12">
-        <p className="font-custom p-5 text-2xl">{caption.Baris1}</p>
-        <p className="py-4">{caption.Baris2}</p>
-        <p className="font-custom py-7">{caption.Baris3}</p>
-        <p className="">{caption.Baris4}</p>
+        <p className="font-custom p-5 text-2xl">{caption.baris1}</p>
+        <p className="py-4">{caption.baris2}</p>
+        <p className="font-custom py-7">{caption.baris3}</p>
+        <p className="">{caption.baris4}</p>
       </div>
       <div className="w-full justify-center flex">
-        <button onClick={randomCaptiion} className="bg-customGreen hover:bg-customOrange p-5 fixed bottom-8 rounded-lg text-white font-bold">
+        <button
+          onClick={getRamdomCaption}
+          className="bg-customGreen hover:bg-customOrange p-5 fixed bottom-8 rounded-lg text-white font-bold"
+        >
           Random Caption untuk memperbaiki mood mu
         </button>
       </div>
