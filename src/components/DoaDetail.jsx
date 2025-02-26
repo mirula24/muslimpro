99e9dd255f85a5699f5718412203b1d0de4f698d
Muhammad Amirul <mirulid@gmail.com>
2025-02-26 23:03:12 +0700

    leading-loose for doa

--- src/components/DoaDetail.jsx
+++ src/components/DoaDetail.jsx
@@ -25,26 +25,30 @@ export default function DoaDetail() {
     console.log(id);
   }, [id]);
 
-
-  const nextID =  useMemo(() => parseInt(id) + 1, [id]);
-  
+  const nextID = useMemo(() => parseInt(id) + 1, [id]);
 
   return (
-      <div className="flex w-full h-screen pt-10 gap-5 flex-col md:px-16 px-2 justify-center ">
-        <div className="bg-black bg-opacity-30 rounded-lg text-white">
-          <h2 className="font-bold text-xl py-10">{doa.doa}</h2>
-          <h2 className="text-2xl pb-10 font-custom">{doa.ayat}</h2>
-          <h2 className="pb-4">{doa.latin}</h2>
-          <h2 className="font-bold pb-10">{doa.artinya}</h2>
-        </div>
-        <div className="w-full items-center space-x-2 text-white">
-          <button onClick={()=>navigate(-1)} className="font-bold p-5 bg-customGreen hover:bg-customOrange hover:text-fuchsia-200 rounded-lg">
-          &#8592; Back
-          </button>
-          <button onClick={() => navigate(`/doa/${nextID}`, { replace: true })} className="font-bold p-5 bg-customGreen hover:bg-customOrange hover:text-fuchsia-200 rounded-lg">
-            Next &#8594;
-          </button>
-        </div>
+    <div className="flex w-full h-screen pt-10 gap-5 flex-col md:px-16 px-2 justify-center ">
+      <div className="bg-black bg-opacity-30 rounded-lg text-white">
+        <h2 className="font-bold text-xl py-10">{doa.doa}</h2>
+        <h2 className="text-2xl pb-10 font-custom leading-loose">{doa.ayat}</h2>
+        <h2 className="pb-4">{doa.latin}</h2>
+        <h2 className="font-bold pb-10">{doa.artinya}</h2>
       </div>
+      <div className="w-full items-center space-x-2 text-white">
+        <button
+          onClick={() => navigate(-1)}
+          className="font-bold p-5 bg-customGreen hover:bg-customOrange hover:text-fuchsia-200 rounded-lg"
+        >
+          &#8592; Back
+        </button>
+        <button
+          onClick={() => navigate(`/doa/${nextID}`, { replace: true })}
+          className="font-bold p-5 bg-customGreen hover:bg-customOrange hover:text-fuchsia-200 rounded-lg"
+        >
+          Next &#8594;
+        </button>
+      </div>
+    </div>
   );
 }
